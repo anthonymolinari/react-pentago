52edce95c0bf365a9743f1353ef3ddab92af1fff
anthonymolinari <molinaria@sonoma.edu>
2023-10-01 15:40:44 -0700

    init commit

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,18 +1,21 @@
-
 import { Box } from "@mui/system";
 
+import { cellSizes } from "../utils/sizes";
 
-export default function Cell(props) {
+export default function Cell( props ) {
 
-    const { data, onClick } = props;
+    const { data, moveHandler } = props;
 
     return (
-        <Box sx={{
+        <Box key={`cell-${data.row}-${data.col}`} sx={{
+            width: cellSizes.width,
+            height: cellSizes.height,
+            backgroundColor: data.backgroundColor,
             border: 1,
             borderColor: 'black',
-            boarderRadius: '50%',
-        }} onClick={}>
-
-        </Box>
-    );
+            borderRadius: '50%',
+            margin: 0.4,
+        }} onClick={() => moveHandler(data)}> </Box>
+    )
 }
+
---
+++ src/utils/cellStyles.js
@@ -0,0 +1,14 @@
+
+const cellStyles = {
+    playerOne: {
+        backgroundColor: 'red',
+    },
+    playerTwo: {
+        backgroundColor: 'black',
+    },
+    unused: {
+        backgroundColor: 'white',
+    },
+}
+
+export default cellStyles;
